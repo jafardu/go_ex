663506a49a397dbc5fdc9175a19139ef6bb7f2e2
duxiao <1610469455@qq.com>
2022-06-13 08:17:04 +0800

    kafka

--- logagent/kafka/kafka.go
+++ logagent/kafka/kafka.go
@@ -1,43 +1,48 @@
 /*
 * @Description:
 * @Author: jafar du
-* @Date: 2022/6/12 19:16
+* @Date: 2022/6/13 7:31
  */
-package main
+package kafka
 
 import (
 	"fmt"
 	"github.com/Shopify/sarama"
 )
 
-// 基于sarama第三⽅库开发的kafka client
-func main() {
+// 专门往kafka写日志的模块
+
+var (
+	client sarama.SyncProducer // 声明一个全局的连接kafka的生产者client
+)
+
+// Init 初始化kafka
+func Init([]string) (err error) {
 	config := sarama.NewConfig()
-	// tailf包使⽤
 	config.Producer.RequiredAcks = sarama.WaitForAll          // 发送完数据需要leader和follow都确认
-	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出⼀个 partition
+	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出一个partition
 	config.Producer.Return.Successes = true                   // 成功交付的消息将在success channel返回
 
-	// 构造⼀个消息
-	msg := &sarama.ProducerMessage{}
-	msg.Topic = "web_log"
-	msg.Value = sarama.StringEncoder("this is a test log")
-
 	// 连接kafka
-	client, err := sarama.NewSyncProducer([]string{"127.0.0.1:9092"}, config)
+	client, err = sarama.NewSyncProducer([]string{"192.168.1.7:9092"}, config)
 	if err != nil {
 		fmt.Println("producer closed, err:", err)
 		return
 	}
-	fmt.Println("连接kafka成功！")
-	defer client.Close()
+	return
+}
+
+func SendToKafka(topic, data string) {
+	// 构造一个消息
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = topic
+	msg.Value = sarama.StringEncoder(data)
+
 	// 发送消息
 	pid, offset, err := client.SendMessage(msg)
-	fmt.Println("xxx")
 	if err != nil {
 		fmt.Println("send msg failed, err:", err)
 		return
 	}
 	fmt.Printf("pid:%v offset:%v\n", pid, offset)
-	fmt.Println("发送成功！")
 }
---
+++ logagent/main.go
@@ -0,0 +1,45 @@
+/*
+* @Description:
+* @Author: jafar du
+* @Date: 2022/6/13 7:32
+ */
+package main
+
+import (
+	"fmt"
+	"go_ex/logagent/kafka"
+	"go_ex/logagent/taillog"
+	"time"
+)
+
+func run() {
+	// 1.读取日志
+	for {
+		select {
+		case line := <-taillog.ReadChan():
+			// 2.发送到kafka
+			kafka.SendToKafka("web_log", line.Text)
+		default:
+			time.Sleep(time.Second)
+		}
+
+	}
+
+}
+
+func main() {
+	// 1.初始化kafka连接
+	err := kafka.Init([]string{"127.0.0.1:9092"})
+	if err != nil {
+		fmt.Println("init kafka failed,err:%v\n", err)
+		return
+	}
+	// 2.打开日志文件准备收集日志
+	err = taillog.Init("./my.log")
+	if err != nil {
+		fmt.Printf("init taillog failed,err :%v\n", err)
+		return
+	}
+	// 3.
+	run()
+}
